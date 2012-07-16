adedc39daefb300f3625b2fedabd890aabb872a3
Olivier Bonaventure <bonaventure@acm.org>
2012-07-16 18:34:13 +0200

    Removed old backup copy

--- book-july2011/application/python/simpleclientip.py
+++
@@ -1,8 +0,0 @@
-# A simple client of the connectionless service
-import socket
-import sys
-HOSTIP=sys.argv[1]
-PORT=int(sys.argv[2])
-MSG="Hello, World!"
-s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM ) 
-s.sendto( MSG, (HOSTIP, PORT) )
--- book-july2011/util/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-
-for f in $( ls -d */*.key | grep -v Backup ); do zip -urp $f.zip $f ; done
-
